e8ee617b41437a81481a3009b540db874f40cece
jc <vaultdweller123@gmail.com>
2013-08-30 09:06:07 +0800

    added author on the readme file

--- jcPagination.js
+++ jcPagination.js
@@ -1,3 +1,13 @@
+/**
+* jcPagination
+*
+* Copyright (c) 2011 Joe Christian Alcomendras
+* Dual licensed under the MIT and GPL licenses:
+* http://www.opensource.org/licenses/mit-license.php
+* http://www.gnu.org/licenses/gpl.html
+*
+*/
+
 var jcPagination = function(option) {
 
 	// set value
